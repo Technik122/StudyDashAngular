54c740ee580f11db9e2ecb41f0db7797811e0d65
Technik122 <66574927+Technik122@users.noreply.github.com>
2024-05-03 01:40:10 +0200

    Hinzufügen von Anfragen an Backend, erste Login/Register Seite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'StudyDashAngular';
+  title: string;
+
+  constructor() {
+    this.title = 'StudyDash';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
+import { AuthContentComponent } from './auth-content/auth-content.component';
+import { AxiosService } from "./axios.service";
+import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatCheckbox} from "@angular/material/checkbox";
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AuthContentComponent,
+    WelcomeContentComponent,
+    LoginFormComponent,
+    ContentComponent,
+    ButtonsComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardContent,
+    MatCardTitle,
+    MatCard,
+    MatCheckbox,
+    MatCardActions,
+    MatCardHeader
   ],
-  providers: [],
+  providers: [AxiosService, provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
