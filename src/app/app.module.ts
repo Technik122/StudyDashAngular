3f15a236d6eaf03e4cbf0c42c126593e9e045df8
Technik122 <66574927+Technik122@users.noreply.github.com>
2024-05-10 09:58:26 +0200

    Löschen der Testkomponenten

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,8 @@ import { AppComponent } from './app.component';
 
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { HeaderComponent } from './header/header.component';
-import { AuthContentComponent } from './auth-content/auth-content.component';
 import { AxiosService } from "./axios.service";
-import { WelcomeContentComponent } from './welcome-content/welcome-content.component';
-import { LoginFormComponent } from './login-form/login-form.component';
 import { ContentComponent } from './content/content.component';
-import { ButtonsComponent } from './buttons/buttons.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -26,12 +21,7 @@ import {MatCheckbox} from "@angular/material/checkbox";
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthContentComponent,
-    WelcomeContentComponent,
-    LoginFormComponent,
     ContentComponent,
-    ButtonsComponent,
     LoginComponent,
   ],
   imports: [
